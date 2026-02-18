6468f64b0ad1880543c8d6edd99d0c815d48cfa6
Paul C. Martin <pmartin@gatech.edu>
2026-02-18 11:58:08 -0500

    Better logging

--- server/db/connect.go
+++ server/db/connect.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/PaulChristophel/agartha/server/config"
+	"github.com/PaulChristophel/agartha/server/logger"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -15,21 +16,47 @@ var DB *gorm.DB
 
 func ConnectToDatabase(options config.DBOptions) {
 	var err error
-	// Connection URL to connect to Postgres Database
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", options.Host, options.Port, options.User, options.Password, options.DBName, options.SSLMode)
 
+	log := logger.GetLogger()
+
+	// Connection URL to connect to Postgres Database
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		options.Host,
+		options.Port,
+		options.User,
+		options.Password,
+		options.DBName,
+		options.SSLMode,
+	)
 	// Retry logic
 	for i := range 3 {
 		// Connect to the DB and initialize the DB variable
 		DB, err = gorm.Open(postgres.Open(dsn))
 		if err == nil {
-			log.Printf("Connection Opened to Database postgres://%s:***@%s:%d/%s?sslmode=%s", options.User, options.Host, options.Port, options.DBName, options.SSLMode)
+			log.Info(
+				"Connection opened to database",
+				zap.String("user", options.User),
+				zap.String("host", options.Host),
+				zap.Int("port", options.Port),
+				zap.String("dbname", options.DBName),
+				zap.String("sslmode", options.SSLMode),
+			)
 			return
 		}
-		log.Printf("Failed to connect to database. Attempt %d/3. Retrying in 5 seconds...", i+1)
+		log.Warn(
+			"Failed to connect to database. Retrying",
+			zap.Int("attempt", i+1),
+			zap.Int("max_attempts", 3),
+			zap.Error(err),
+		)
 		time.Sleep(5 * time.Second)
 	}
 
 	// If still unable to connect after 3 attempts, panic
-	panic(fmt.Sprintf("failed to connect to database after 3 attempts: %v", err))
+	log.Fatal(
+		"Failed to connect to database after max attempts",
+		zap.Int("max_attempts", 3),
+		zap.Error(err),
+	)
 }
